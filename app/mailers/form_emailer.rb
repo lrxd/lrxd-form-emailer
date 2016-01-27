a77060a7615712a425a70f17ac67edef16484116
Tyler Gerard <tgerard@lrxd.com>
2016-01-27 16:30:32 -0700

    Moved to email into .env.

--- app/mailers/form_emailer.rb
+++ app/mailers/form_emailer.rb
@@ -1,6 +1,6 @@
 class FormEmailer < ApplicationMailer
   default :from => 'noreply@lrxd.com'
-  default :to => 'tgerard@lrxd.com'
+  default :to => ENV['SEND_TO_EMAIL']
 
   def send_form_email(form_name, form_url, form_data)
     @form_name = form_name
